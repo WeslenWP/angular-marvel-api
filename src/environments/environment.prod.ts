770cbb7d9cdac50880de7fe0cb93dbf6d289b277
WeslenWP <57677674+WeslenWP@users.noreply.github.com>
2022-04-20 15:39:01 -0300

    Removing ignored files

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  //get your development keys from https://developer.marvel.com/
-  privateK: 'Your private key',
-  publicK: 'Your public key',
-};
