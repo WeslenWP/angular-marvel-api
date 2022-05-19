5c0cd29190bb8a1121c5b45bbe04c8aadaaa0ac7
Weslen <weslen1905@gmail.com>
2022-05-19 04:35:24 -0300

    Agora a informação vem do BehaviorSubject de Characters

--- src/app/modules/characters/characters.service.ts
+++ src/app/modules/characters/characters.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, Observable, of, BehaviorSubject, switchMap, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 import { Md5 } from 'ts-md5/dist/md5';
@@ -9,22 +9,35 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class CharactersService {
-  privateK: string = environment.privateK;
-  publicK: string = environment.publicK;
+
   timestamp: number = new Date().getTime();
   hash: string;
   url: string = 'https://gateway.marvel.com/v1/public/characters';
 
+  characters = new BehaviorSubject<any>(null);
+
+  public haveRequest: boolean = false;
+  public searching: boolean = false;
+
+  get characters$() {
+    return this.characters.asObservable()
+  }
+
   constructor(private http: HttpClient) {
-    this.hash = Md5.hashStr(this.timestamp + this.privateK + this.publicK);
+    this.hash = Md5.hashStr(
+      this.timestamp + environment.privateK + environment.publicK
+    );
   }
 
-  getAllCharacters(offset: number, limit: number): Observable<any> {
-    const finalUrl = `${this.url}?ts=${this.timestamp}&apikey=${this.publicK}&hash=${this.hash}&limit=${limit}&offset=${offset}`;
-    return this.http.get(finalUrl);
-  }
+  getAllCharacters(offset: number, limit: number) {
+    this.haveRequest = true
+    const finalUrl = `${this.url}?ts=${this.timestamp}&apikey=${environment.publicK}&hash=${this.hash}&limit=${limit}&offset=${offset}`;
+    const result: any = this.http.get(finalUrl);
 
-  private log(message: string) {
-    console.log(`Error: ${message}`);
+    let subject = result.pipe(tap(() => { this.haveRequest = false }))
+      .subscribe((res: any) => this.characters.next(res.data.results))
+
+
+    // this.characters.next(this.charact ers.getValue() + result);
   }
 }
