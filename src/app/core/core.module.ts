62ebe0d3e02d9b1d195e92dd48f05041b5e80bd0
Weslen <weslen1905@gmail.com>
2022-05-19 05:09:30 -0300

    remoçao do interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,7 +5,7 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoadingComponent } from './components/loading/loading.component';
-import { LoadingInterceptor } from './interceptors/loading.interceptor';
+
 
 const Components = [ToolbarComponent, SidenavComponent, LoadingComponent];
 const Modules = [MaterialModule, HttpClientModule];
@@ -14,12 +14,6 @@ const Modules = [MaterialModule, HttpClientModule];
   declarations: [Components],
   imports: [CommonModule, Modules],
   exports: [Components, Modules],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: LoadingInterceptor,
-      multi: true,
-    },
-  ],
+
 })
-export class CoreModule {}
+export class CoreModule { }
