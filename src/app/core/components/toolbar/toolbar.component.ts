6b37c2d33eb245e5695f6625323ce23a22df245a
Weslen <weslen1905@gmail.com>
2022-05-19 05:10:36 -0300

    Função de search

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
+import { CharactersService } from 'src/app/modules/characters/characters.service';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ToolbarComponent implements OnInit {
-  constructor(private sidenav: AppComponent) {}
+  constructor(private sidenav: AppComponent, private _charactersService: CharactersService) { }
   height: any;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   toggleSideNav() {
     this.sidenav.toggle();
   }
+
+
+  timer: any;
+  search(inputSearch: HTMLInputElement) {
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      this._charactersService.searchCharacters(inputSearch.value)
+    }, 200)
+
+  }
+
 }
