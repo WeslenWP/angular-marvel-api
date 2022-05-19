cf4c31ac4b5df348436f2ef2fa58a9b8619de948
Weslen <weslen1905@gmail.com>
2022-05-19 05:55:51 -0300

    Mudança de aparencia e onde aparece

--- src/app/core/components/loading/loading.component.ts
+++ src/app/core/components/loading/loading.component.ts
@@ -3,15 +3,11 @@ import { LoadingService } from '../../services/loading.service';
 
 @Component({
   selector: 'app-loading',
-  template: `
-    <div *ngIf="active" class="load-container">
-      <mat-spinner color="warn"></mat-spinner>
-    </div>
-  `,
+  template: `<mat-progress-bar color="warn" *ngIf="active" class="loader" mode="indeterminate"></mat-progress-bar>`,
   styleUrls: ['./loading.component.scss'],
 })
 export class LoadingComponent {
-  active: boolean = false;
+  active: boolean = true;
 
   constructor(public _loadingService: LoadingService) {
     this._loadingService.loading$.subscribe((res) => {
