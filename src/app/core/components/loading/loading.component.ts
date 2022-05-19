c5aa8770bdd7de8cdf2e8cbe5bee8bd1d2aaf0d0
Weslen <weslen1905@gmail.com>
2022-05-19 05:10:10 -0300

    Integração com a loading service

--- src/app/core/components/loading/loading.component.ts
+++ src/app/core/components/loading/loading.component.ts
@@ -4,12 +4,18 @@ import { LoadingService } from '../../services/loading.service';
 @Component({
   selector: 'app-loading',
   template: `
-    <div class="load-container">
+    <div *ngIf="active" class="load-container">
       <mat-spinner color="warn"></mat-spinner>
     </div>
   `,
   styleUrls: ['./loading.component.scss'],
 })
 export class LoadingComponent {
-  constructor(public loadingService: LoadingService) {}
+  active: boolean = false;
+
+  constructor(public _loadingService: LoadingService) {
+    this._loadingService.loading$.subscribe((res) => {
+      this.active = res
+    })
+  }
 }
