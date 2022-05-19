fd7f748f1c8de008116906ca078255e006b5e792
Weslen <weslen1905@gmail.com>
2022-05-19 05:48:07 -0300

    Remoção sidenav

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoadingComponent } from './components/loading/loading.component';
 
 
-const Components = [ToolbarComponent, SidenavComponent, LoadingComponent];
+const Components = [ToolbarComponent, LoadingComponent];
 const Modules = [MaterialModule, HttpClientModule];
 
 @NgModule({
